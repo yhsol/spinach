5cd8bb98171645e3a5923f5b05866dbf4179ccc0
yhsol <loshy244110@gmail.com>
2023-06-27 12:00:15 +0900

    feat: Auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,5 @@
-import { Profile } from "@/components/TourOfTailwind";
-import {
-  FirstLetter,
-  Form,
-  ItemPost,
-  SelectItem,
-} from "@/components/TourOfTailwind";
+import Enter from "./enter";
 
 export default function Home() {
-  return (
-    // 메뉴얼로 다크모드 설정할 땐 이렇게 감싸져 있는 엘리먼트에 dark 클래스를 추가해줘야 한다.
-    <div className="dark bg-slate-400 p-20 min-h-screen grid gap-10 lg:grid-cols-2 xl:grid-cols-3 xl:place-content-center">
-      <SelectItem />
-      <Profile />
-      {/* <ItemPost />
-      <Form /> */}
-      <FirstLetter />
-    </div>
-  );
+  return <Enter />;
 }
